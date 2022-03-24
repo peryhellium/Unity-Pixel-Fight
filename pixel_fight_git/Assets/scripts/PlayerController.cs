89251fad61dfebda70e79d2963a0ebb2d96d9002
Majkel <michal.krakow7@gmail.com>
2022-03-24 22:16:40 +0100

    keyboard weapon switch

--- pixel_fight_git/Assets/scripts/PlayerController.cs
+++ pixel_fight_git/Assets/scripts/PlayerController.cs
@@ -177,7 +177,13 @@ public class PlayerController : MonoBehaviour
             SwitchGun();
         }
 
-        
+        for(int i = 0; i <allGuns.Length; i++)
+        {
+            if(Input.GetKeyDown((i + 1).ToString())) {
+                selectedGun = i;
+                SwitchGun();
+            }
+        }
 
 
         if (Input.GetKeyDown(KeyCode.Escape))
