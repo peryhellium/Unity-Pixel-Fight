910b24275e6c8493323621e7b5fff1243f1a2716
Majkel <michal.krakow7@gmail.com>
2022-07-06 12:50:25 +0200

    setting up events in PUN

--- pixel_fight_git/Assets/scripts/MatchManager.cs
+++ pixel_fight_git/Assets/scripts/MatchManager.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
-public class MatchManager : MonoBehaviour
+public class MatchManager : MonoBehaviourPunCallbacks, IOnEventCallback
 {
     public static MatchManager instance;
 
@@ -13,6 +15,15 @@ public class MatchManager : MonoBehaviour
         instance = this;
     }
 
+    public enum EventCodes : byte
+    {
+        NewPlayer,
+        ListPlayers,
+        UpdateStat
+    }
+
+    public List<PlayerInfo> allPlayers = new List<PlayerInfo>();
+    private int index;
     void Start()
     {
         if (!PhotonNetwork.IsConnected)
@@ -26,4 +37,88 @@ public class MatchManager : MonoBehaviour
     {
         
     }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        //Codes above 200 are reserved by PUN
+        if(photonEvent.Code < 200)
+        {
+            EventCodes theEvent = (EventCodes)photonEvent.Code;
+            object[] data = (object[])photonEvent.CustomData;
+
+            switch (theEvent)
+            {
+                case EventCodes.NewPlayer:
+                    NewPlayerReceive(data);
+                    break;
+
+                case EventCodes.ListPlayers:
+                    NewPlayerReceive(data);
+                    break;
+
+                case EventCodes.UpdateStat:
+                    NewPlayerReceive(data);
+                    break;
+            }
+        }
+    }
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public void NewPlayerSend()
+    {
+
+    }
+
+    public void NewPlayerReceive(object[] dataReceived)
+    {
+
+    }
+
+
+    public void ListPlayersSend()
+    {
+
+    }
+
+    public void ListPlayersReceive(bject[] dataReceived)
+    {
+
+    }
+
+    public void UpdateStatsSend()
+    {
+
+    }
+
+    public void UpdateStatsReceive(bject[] dataReceived)
+    {
+
+    }
+}
+
+
+
+[System.Serializable]
+public class PlayerInfo
+{
+    public string name;
+    public int actor, kills, deaths;
+
+    public PlayerInfo(string _name, int _actor, int _kills, int _deaths)
+    {
+        name = _name;
+        actor = _actor;
+        kills = _kills;
+        deaths = _deaths;
+    }
+
 }
