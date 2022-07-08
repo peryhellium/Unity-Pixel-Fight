0b63c867090aa1d6ef0b1d3d8f4fa6a249f96e0a
xNavya <navulka@gmail.com>
2022-07-08 17:48:16 +0200

    aktualizacja main menu

---
+++ pixel_fight_git/Assets/scripts/CameraSetup.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetup : MonoBehaviour
+{
+	Animator CameraObject;
+
+    private void Start()
+    {
+		CameraObject = transform.GetComponent<Animator>();
+	}
+
+
+    public void Position2()
+    {
+        CameraObject.SetFloat("Play", 1);
+    }
+
+    public void Position1()
+    {
+        CameraObject.SetFloat("Play", 0);
+    }
+
+    public void Position4()
+    {
+        CameraObject.SetFloat("Settings", 1);
+    }
+
+    public void Position3()
+    {
+        CameraObject.SetFloat("Settings", 0);
+    }
+
+}
